39469b81d2acfaf873b1a32e079ea0a954206f59
farshad ahangari <farshad.ahg@gmail.com>
2019-05-05 22:56:37 +0430

    set up modal

--- src/components/Support/index.js
+++ src/components/Support/index.js
@@ -1,12 +1,52 @@
 import React, { Component } from "react";
-import { Container, Text } from "native-base";
+import { Button, Text, View } from "react-native";
+import Modal from "react-native-modal";
 
 export default class Support extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isModalVisible: false
+    };
+  }
+
+  renderModalContent = () => (
+    <View>
+      <Text style={styles.contentTitle}>Hi 👋!</Text>
+      <Button
+        onPress={() => this.toggleModal()}
+        title="Close"
+      />
+    </View>
+  );
+
+  toggleModal = () => {
+    this.setState({ isModalVisible: !this.state.isModalVisible });
+  };
+
   render() {
     return (
-        <Container>
-            <Text>supprot screen</Text>    
-        </Container>
+      <View style={{ flex: 1 }}>
+        <Button title="Show modal" onPress={this.toggleModal} />
+        <Modal 
+          isVisible={this.state.isModalVisible}
+          onBackdropPress={this.toggleModal}
+          onBackButtonPress={this.toggleModal}
+          style={{ justifyContent: 'flex-end', margin: 0 }}
+        >
+          <View style={{ 
+            backgroundColor:'white',
+            borderTopLeftRadius: 15,
+            borderTopRightRadius: 15,
+            padding: 22,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 4,
+            borderColor: 'rgba(0, 0, 0, 0.1)'}}>
+            {this.renderModalContent()}
+          </View>
+        </Modal>
+      </View>
     );
   }
 }
