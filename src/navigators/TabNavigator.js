7e9d6c1a74cc5b4455c528b0ac6c7d17542ac848
farshad ahangari <farshad.ahg@gmail.com>
2019-05-05 21:40:22 +0430

    change footer tab styles

--- src/navigators/TabNavigator.js
+++ src/navigators/TabNavigator.js
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from "react-navigation";
 import i18n from '../i18n';
 import { Button, Text, Icon, Footer, FooterTab } from "native-base";
 import { ProfileNavigator, HomeNavigator, SupportNavigator } from './StackNavigator';
+import styles from './styles';
 
 export default TabNavigator = createBottomTabNavigator(
   {
@@ -28,27 +29,30 @@ export default TabNavigator = createBottomTabNavigator(
     tabBarComponent: props => {
       return (
         <Footer>
-        <FooterTab>
+        <FooterTab style={styles.footerTab}>
           <Button
+            style={props.navigation.state.index === 2 ? styles.activeBtn : null}
             vertical
             active={props.navigation.state.index === 2}
             onPress={() => props.navigation.navigate("Support")}>
-            <Icon name="chatboxes" />
-            {props.navigation.state.index === 2 && (<Text>{ i18n.t('mainTab.support') }</Text>)}
+            <Icon style={props.navigation.state.index === 2 ? styles.activeIconAndTxt : styles.inactiveIcon} name="chatboxes" />
+            {props.navigation.state.index === 2 && (<Text style={props.navigation.state.index === 2 ? styles.activeIconAndTxt : null}>{ i18n.t('mainTab.support') }</Text>)}
           </Button>
           <Button
+            style={props.navigation.state.index === 1 ? styles.activeBtn : null}
             vertical
             active={props.navigation.state.index === 1}
             onPress={() => props.navigation.navigate("Profile")}>
-            <Icon name="person" />
-            {props.navigation.state.index === 1 && (<Text>{ i18n.t('mainTab.profile') }</Text>)}
+            <Icon style={props.navigation.state.index === 1 ? styles.activeIconAndTxt : styles.inactiveIcon} name="person" />
+            {props.navigation.state.index === 1 && (<Text style={props.navigation.state.index === 1 ? styles.activeIconAndTxt : null}>{ i18n.t('mainTab.profile') }</Text>)}
           </Button>
           <Button
+            style={props.navigation.state.index === 0 ? styles.activeBtn : null}
             vertical
             active={props.navigation.state.index === 0}
             onPress={() => props.navigation.navigate("Home")}>
-            <Icon name="home" />
-            {props.navigation.state.index === 0 && (<Text>{ i18n.t('mainTab.home') }</Text>)}
+            <Icon style={props.navigation.state.index === 0 ? styles.activeIconAndTxt : styles.inactiveIcon} name="home" />
+            {props.navigation.state.index === 0 && (<Text style={props.navigation.state.index === 0 ? styles.activeIconAndTxt : null}>{ i18n.t('mainTab.home') }</Text>)}
           </Button>
         </FooterTab>
       </Footer>
---
+++ src/navigators/styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+export default styles = StyleSheet.create({
+  footerTab: {
+    backgroundColor: '#f2f2ff',
+    borderTopColor: '#e3e3e3',
+    borderTopWidth: 1
+  },
+  activeBtn: {
+    backgroundColor: '#f2f2ff'
+  },
+  activeIconAndTxt: {
+    color: '#00bd56',
+    fontWeight: 'bold'
+  },
+  inactiveIcon: {
+    color: '#747474'
+  }
+});
